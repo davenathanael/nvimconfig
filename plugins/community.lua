75dc944008765660eb753bb4eafd5c71db7abf6b
Dave Nathanael <davenathanaeld@gmail.com>
2024-03-11 22:17:55 +0800

    Add community packs

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,7 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.html-css" },
 }
